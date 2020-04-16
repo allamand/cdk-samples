f5e716369ab6edc5def44cd02fed038563c27209
Pahud <pahudnet@gmail.com>
2020-04-16 18:58:16 +0800

    add eks.ts

---
+++ typescript/lib/eks.ts
@@ -0,0 +1,31 @@
+import cdk = require('@aws-cdk/core');
+import eks = require('@aws-cdk/aws-eks');
+import ec2 = require('@aws-cdk/aws-ec2');
+import iam = require('@aws-cdk/aws-iam');
+
+export class EksStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = this.node.tryGetContext('use_default_vpc') ? ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true }) :
+      new ec2.Vpc(this, 'Vpc', {
+        maxAzs: 3,
+        natGateways: 1
+      });
+
+    const mastersRole = new iam.Role(this, 'AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
+    });
+
+    const cluster = new eks.Cluster(this, 'EKSCluster', {
+      vpc,
+      mastersRole,
+      version: '1.15',
+      defaultCapacity: 2,
+      defaultCapacityInstance: new ec2.InstanceType('t3.large')
+    });
+
+    new cdk.CfnOutput(this, 'Region', { value: this.region })
+  }
+}
+
